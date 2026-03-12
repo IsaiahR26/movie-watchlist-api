a31e04d47454c8041d9715a6f0fcf21337ab6bc2
IsaiahR26 <irrodri@pointpark.edu>
2026-03-12 13:48:08 -0400

    Added GET Movies that filter the movies

--- server.js
+++ server.js
@@ -25,4 +25,35 @@ app.get ("/", (req, res) => {
     res.send("Movie Watchlist API is running");
 });
 
+/* GET MOVIES */
 
+app.get("/api/v1/movies", async (req, res) => {
+    try {
+        const { stuatus, favorite } = req.query;
+
+        let query = "SELECT * FROM movies";
+        const values = [];
+        const conditions = [];
+
+        if (status) {
+            values.push(status);
+            conditions.push(`status = $${values.length}`);
+        }
+
+        if (favorite !== undefined) {
+            values.push(favorite === "true");
+            conditions.push(`is_favorite = $${values.length}`);
+        }
+
+        if (conditions.length > 0) {
+            query += " WHERE " + conditions.join(" AND ");
+        }
+
+        const result = await pool.query(query,values);
+        res.json(result.rows);
+    
+    }   catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error fetching movies"});
+    }
+});
