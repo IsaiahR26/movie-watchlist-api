5a6453975a246fd6f5079f15a88cd327dc09c67c
IsaiahR26 <irrodri@pointpark.edu>
2026-03-12 21:25:16 -0400

    Added get by id route

--- server.js
+++ server.js
@@ -66,6 +66,19 @@ app.get("/api/v1/movies", async (req, res) => {
     }
 });
 
+/* GET DATA BY ID */
+
+app.get("/api/v1/movies/:id", async (req, res) => {
+    const { id } = req.params;
+
+    const result = await pool.query(
+        "SELECT * FROM movies WHERE id = $1",
+        [id]
+    );
+
+    res.json(result.rows[0]);
+});
+
 /* POST MOVIES (Basically adds movies to the GET MOVIES list) */
 
 app.post("/api/v1/movies", async (req, res) => {
